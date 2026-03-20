ac621e4dc5c2d57186d8aff9d56c982842604de3
Mostafa Farrag <moah.farag@gmail.com>
2026-03-20 23:11:53 +0100

    reformat

--- src/statista/__init__.py
+++ src/statista/__init__.py
@@ -2,8 +2,6 @@
 
 from importlib.metadata import PackageNotFoundError, version
 
-from statista.exceptions import ParameterError, StatistaError
-
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:  # pragma: no cover
