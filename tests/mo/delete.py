f162fb273b5c770fcfae2b844e3bbe15658ce74c
Mostafa Farrag <moah.farag@gmail.com>
2026-04-15 21:20:49 +0200

    remove

--- tests/mo/delete.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-from statista.confidence_interval import ConfidenceInterval
-from statista.descriptors import rmse_hf
-
-data = [3.1, 2.4, 5.6, 8.4]
-indeces = ConfidenceInterval.bs_indexes(data, n_samples=2)
-[i for i in indeces]
-next(indeces)
